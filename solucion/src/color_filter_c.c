c5911074cf71677f5f276346c65b8076b1ee8173
ealmansi <ealmansi@gmail.com>
2013-09-11 20:15:16 -0300

    Filtro color en C versión elemental, pasa los tests fcolor

--- solucion/src/color_filter_c.c
+++ solucion/src/color_filter_c.c
@@ -1,8 +1,6 @@
 #include "utils.h"
 #include "tiempo.h"
 
-#define 		MAX_THRESHOLD 		442				// techo(raiz(3) * 255)
-
 void color_filter_c(unsigned char *src,
                     unsigned char *dst,
                     unsigned char rc,
@@ -12,27 +10,24 @@ void color_filter_c(unsigned char *src,
                     int width,
                     int height)
 {
-	TIMER_BEGIN();
+	// TIMER_BEGIN();
 
-	threshold = MIN(MAX(0, threshold), MAX_THRESHOLD);
-	int threshold_2 = threshold * threshold;
-
-	TIMER_PRINT_STATUS("antes_del_ciclo");
-	unsigned char r, g, b, diff_r, diff_g, diff_b;
+	// TIMER_PRINT_STATUS("antes_del_ciclo");
+	int r, g, b, diff_r, diff_g, diff_b, valor;
 	for (int i = 0; i < 3 * width * height; i += 3)
 	{
-		diff_r = (r = src[i + 0]) - rc;
+		diff_r = (r = src[i + 2]) - rc;
 		diff_g = (g = src[i + 1]) - gc;
-		diff_b = (b = src[i + 2]) - bc;
+		diff_b = (b = src[i + 0]) - bc;
 
-		if(diff_r * diff_r + diff_g * diff_g + diff_b * diff_b > threshold_2)
+		if(diff_r * diff_r + diff_g * diff_g + diff_b * diff_b > threshold)
 			r = g = b = ((r + g + b) / 3);
 
-		dst[i + 0] = r;
+		dst[i + 2] = r;
 		dst[i + 1] = g;
-		dst[i + 2] = b;
+		dst[i + 0] = b;
 	}
-	TIMER_PRINT_STATUS("despues_del_ciclo");
+	// TIMER_PRINT_STATUS("despues_del_ciclo");
 
-	TIMER_END();
+	// TIMER_END();
 }

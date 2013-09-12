2207d8d6e4f89783ea533ec1671b7bcd495002b0
Miguel Duarte <miguelfeliped@gmail.com>
2013-09-12 13:50:26 -0300

    decode asm muy avanzado. No anda, pero estoy seguro de que es una boludez.

--- solucion/src/decode_asm.asm
+++ solucion/src/decode_asm.asm
@@ -2,6 +2,16 @@ global decode_asm
 
 section .data
 
+align 16
+__mascara_01: DQ 0x03030303, 0x03030303
+__mascara_23: DQ 0x0C0C0C0C, 0x0C0C0C0C
+__mascara_unos: DB 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+
+__mascara_sumar_1: DB 0x04,0x04,0x04,0x04,0x04,0x04,0x04,0x04,0x04,0x04,0x04,0x04,0x04,0x04,0x04,0x04
+__mascara_restar_1: DB 0x08,0x08,0x08,0x08,0x08,0x08,0x08,0x08,0x08,0x08,0x08,0x08,0x08,0x08,0x08,0x08
+__mascara_invertir: DB  0x0c,0x0c,0x0c,0x0c,0x0c,0x0c,0x0c,0x0c,0x0c,0x0c,0x0c,0x0c,0x0c,0x0c,0x0c,0x0c
+
+__mascara_filtrar: DD 0xFF000000,0xFF000000,0xFF000000,0xFF000000
 
 section .text
 ;void decode_asm(unsigned char *src,
@@ -10,4 +20,118 @@ section .text
 ;              int height);
 
 decode_asm:
+	PUSH rbp; Alineada
+	MOV rbp, rsp;
+	PUSH rbx;
+
+	xor r9,r9; r9 va a ser el contador.
+	xor r10, r10
+
+	;______Se traen las máscaras a registro
+	MOVDQA xmm15, [__mascara_01];
+	MOVDQA xmm14, [__mascara_23];
+	MOVDQA xmm13, [__mascara_unos];
+	MOVDQA xmm12, [__mascara_sumar_1];
+	MOVDQA xmm11, [__mascara_restar_1];
+	MOVDQA xmm10, [__mascara_invertir];
+	MOVDQA xmm9, [__mascara_filtrar];
+
+	
+	ciclo:
+	MOVDQU xmm0, [rdi+r9];
+	MOVDQA xmm1, xmm0;
+
+	PAND xmm0, xmm15; en xmm0 quedan los bits 0 y 1.
+	PAND xmm1, xmm14; en xmm1 quedan los bits 3 y 2.
+
+	MOVDQA xmm2, xmm1; Copio para no perder datos.
+
+	PCMPEQB xmm2, xmm12; Esto produce en xmm2 una máscara con 1's donde hay que sumar 1.
+	PAND xmm2, xmm13; ahora en xmm2 hay un uno sólo onde hay que sumar 1.
+	PADDB xmm0, xmm2; Se suma 1 donde hace falta.
+	PAND xmm0, xmm15; Para que no quede nada raro.
+
+	MOVDQA xmm2, xmm1;
+	
+	PCMPEQB xmm2, xmm11; Se verifica cuales indican que hay que restar 1.
+	PAND xmm2, xmm13; Ahora hay un uno sólo donde hay que restar 1.
+	PSUBB xmm0, xmm2; Se resta 1 donde corresponde.
+	PAND xmm0, xmm15; Para que no quede nada raro.
+
+	PCMPEQB xmm1, xmm10;
+	PAND xmm1, xmm10;
+	PXOR xmm0, xmm1; Se invierten los lugares correspondientes.
+
+	
+	MOVDQA xmm1, xmm0;
+	MOVDQA xmm2, xmm0;
+	MOVDQA xmm3, xmm0;
+
+	PSLLDQ xmm1, 1;
+	PSLLDQ xmm2, 2;
+	PSLLDQ xmm3, 3;
+
+	PSLLD xmm1, 2;
+	PSLLD xmm2, 4;
+	PSLLD xmm3, 6;
+
+	PADDB xmm0, xmm1;
+	PADDB xmm0, xmm2;
+	PADDB xmm0, xmm3;
+
+	PAND xmm0, xmm9;
+
+	PEXTRB rbx, xmm0, 15 ;
+	MOV al, bl;
+	
+	CMP al, 0 ;
+		JNE continuar_ciclo
+		MOV [rsi+r10], al ;
+		JMP salida
+
+continuar_ciclo:
+	SHL rax, 8;
+
+	PEXTRB rbx, xmm0, 00001011b ;
+	MOV al, bl;
+	CMP al, 0 ;
+		JNE continuar_ciclo2
+		MOV [rsi+r10], ax ;
+		JMP salida
+	
+continuar_ciclo2:
+	
+	PEXTRB rbx, xmm0, 00000111b;
+
+	CMP bl, 0 ;
+		JNE continuar_ciclo3;
+		MOV [rsi+r10], ax;
+		ADD r10, 2;
+		MOV [rsi+r10], bl;
+		JMP salida;
+
+continuar_ciclo3:
+
+	SHL rax, 8;
+	MOV al, bl;
+	SHL rax, 8
+
+	PEXTRB rbx, xmm0, 00000000b;
+	MOV al, bl;
+	MOV [rsi+r10], eax; Esto lo voy a tener que grabar si o si.
+	CMP al, 0 ;
+		JE salida;
+		
+
+continuar_ciclo4:
+
+	ADD r9, 16 ;
+	ADD r10, 4;
+
+	CMP r9d, r8d;
+	JL ciclo;
+
+salida:
+	POP rbx
+	POP rbp
     ret
--- solucion/src/decode_c.c
+++ solucion/src/decode_c.c
@@ -9,23 +9,24 @@ void decode_c(unsigned char* src,
               int width,
               int height)
 {
-	
-	int j=0;
-	int k=0;
 
-	while (1) {
 
-		char partial_result = 0;
-		for(int bit_shift=0 ; bit_shift <=6 ; bit_shift+=2){
-			char a,b;
+	int j=0;// Contador para src
+	int k=0;// Contador para code. no pueden ser el mismo porque este avanza 4 veces mas lento.
+
+	while (1) { //La única manera de salir del ciclo es que el string termine, como es de C se sabe que termina en NULL
+
+		char partial_result = 0; // partial_result es un byte que se va a ir llenando de a poco.
+		for(int bit_shift=0 ; bit_shift <=6 ; bit_shift+=2){ //bit_shift indica cuantos bits debe moverse a izquierda el bit actual. Se aumenta de a 2 y va de 0 a 6
+			char a,b; // Se van a nacesitar 2 copias del byte
 			a = src[j];
 
 			b = a;
 
-			a = a & _mask_23;
-			b = b & _mask_01;
+			a = a & _mask_23; //Una para los bits 2 y 3
+			b = b & _mask_01; // y otra para los bits 0 y 1
 
-			if (a==0x04){
+			if (a==0x04){ //Según el valor de a se decide que hacer.
 				b=b+1;
 				b=b & _mask_01;
 			}
@@ -37,13 +38,13 @@ void decode_c(unsigned char* src,
 			if (a== 0xc){
 				b= b ^ _negation_mask;
 			}
-			b = b << bit_shift;
-			partial_result = partial_result + b;
-			j++;
+			b = b << bit_shift; //Se mueven los bits 1 y 0 al lugar correspondiente
+			partial_result = partial_result + b; //se acumula el cacho de biy en partial result
+			j++; //Se avanza el contador de src.
 		}
-		code[k]= partial_result;
+		code[k]= partial_result; //Una vez que se tiene una partial result se lo acumula en code
 		k++;
-		if(partial_result==0){
+		if(partial_result==0){ //Si se llegó al null del final del string se terminó.
 			break;
 		}
 	}

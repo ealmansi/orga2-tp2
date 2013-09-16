73e6963d1b09e693bb1603e81e6f75e1c0f264fc
ealmansi <ealmansi@gmail.com>
2013-09-16 04:22:35 -0300

    Fcolor asm WIP (le estoy agarrando la mano) + un .asm con handys

--- solucion/src/color_filter_asm.asm
+++ solucion/src/color_filter_asm.asm
@@ -1,17 +1,132 @@
+; RDI,RSI,RDX,RCX,R8,R9
+; XMM0,XMM1,XMM2,XMM3,XMM4,XMM5,XMM6,XMM7
+; Preservar RBX, R12, R13, R14, R15
+; resultado en RAX o XMM0
+; Byte: AL, BL, CL, DL, DIL, SIL, BPL, SPL, R8L - R15L
+; Word: AX, BX, CX, DX, DI, SI, BP, SP, R8W - R15W
+; DWord: EAX, EBX, ECX, EDX, EDI, ESI, EBP, ESP, R8D - R15D
+; QWord: RAX, RBX, RCX, RDX, RDI, RSI, RBP, RSP, R8 - R15
+
+;	;	;	;	;	Exporta	;	;	;	;	;	;
+
 global color_filter_asm
 
-section .data
+;	;	;	;	;	Externas ;	;	;	;	;	;
 
+extern printf
+
+;	;	;	;	;	Includes ;	;	;	;	;	;
+
+%include "handy.asm"
+
+;	;	;	;	;	Datos	;	;	;	;	;	;
+
+align 16
+mascara_procesar_dos_pixeles_1: DW 0xFFFF, 0x0000, 0x0000, 0xFFFF, 0x0000, 0x0000, 0x0000, 0x0000
+mascara_procesar_dos_pixeles_2: DW 0xFFFF, 0x0000, 0x0000, 0xFFFF, 0x0000, 0x0000, 0x0000, 0x0000
+
+;	;	;	;	;	Macros ;	;	;	;	;	;
+
+%macro generar_mascara_color_patron 0
+	pinsrw 		XMM14, R12W, 0
+	pinsrw 		XMM14, R11W, 1
+	pinsrw 		XMM14, R10W, 2
+	pinsrw 		XMM14, R12W, 3
+	pinsrw 		XMM14, R11W, 4
+	pinsrw 		XMM14, R10W, 5
+	pinsrw 		XMM14, R12W, 6
+	pinsrw 		XMM14, R11W, 7
+%endmacro
+
+%macro generar_mascara_threshold 0
+	pinsrw 		XMM15, R13W, 0
+	pinsrw 		XMM15, R13W, 1
+	pinsrw 		XMM15, R13W, 2
+	pinsrw 		XMM15, R13W, 3
+	pinsrw 		XMM15, R13W, 4
+	pinsrw 		XMM15, R13W, 5
+	pinsrw 		XMM15, R13W, 6
+	pinsrw 		XMM15, R13W, 7
+%endmacro
+
+%macro procesar_dos_pixeles 0
+	debug_string msj_debug
+
+	movdqa 		XMM2, XMM1 					; computo el cuadrado de la diferencia en
+	psubw 		XMM2, XMM14					; cada canal
+	pmullw 		XMM2, XMM2
+
+	movdqa 		XMM3, XMM2					; computo la suma de los tres canales
+	movdqa 		XMM4, XMM2 					; para cada pixel
+	psrldq 		XMM3, 2
+	psrldq 		XMM4, 4
+	paddw 		XMM2, XMM3
+	paddw 		XMM2, XMM4
+
+	pcmpgtw 	XMM2, XMM15
+%endmacro
+
+;	;	;	;	;	Código	;	;	;	;	;	;
 
 section .text
-;void color_filter_asm(unsigned char *src,
-;                    unsigned char *dst,
-;                    unsigned char rc,
-;                    unsigned char gc,
-;                    unsigned char bc,
-;                    int threshold,
-;                    int width,
-;                    int height);
+
+; void color_filter_asm(unsigned char *src,
+;                       unsigned char *dst,
+;                       unsigned char rc,
+;                       unsigned char gc,
+;                       unsigned char bc,
+;                       int threshold,
+;                       int width,
+;                       int height);
 
 color_filter_asm:
+	push_regs
+
+	xor 		R10, R10
+	xor 		R11, R11
+	xor 		R12, R12
+	mov 		R10B, DL									; rc
+	mov 		R11B, CL									; gc
+	mov 		R12B, R8B									; bc
+	mov 		R13D, R9D									; threshold
+	mov 		R14D, [RSP + OFFSET_OPS_STACK]				; width
+	mov 		R15D, [RSP + OFFSET_OPS_STACK + 8]			; height
+	mov 		R8, RDI										; src
+	mov 		R9, RSI										; dst
+
+	imul 		R14, R15 									; R14 <- 3 * width * height
+	imul 		R14, 3
+
+	generar_mascara_color_patron							; queda en XMM14
+	generar_mascara_threshold								; queda en XMM15
+
+.for:
+	xor 		RCX, RCX									; RCX <- 0, indice del ciclo
+.cond:
+	cmp 		RCX, 1
+	jge 		.endfor
+
+	movdqu 		XMM0, [R8 + RCX]
+
+	movdqa 		XMM1, XMM0 									; dejo en XMM1 los primeros dos pixeles
+	pxor 		XMM2, XMM2									; transformados en words
+	punpckhbw	XMM1, XMM2
+
+	procesar_dos_pixeles									; modifica XMM0
+
+	movdqa 		XMM1, XMM0									; dejo en XMM1 los proximos dos pixeles
+	pslldq 		XMM1, 6										; transformados en words
+	pxor 		XMM2, XMM2
+	punpckhbw	XMM1, XMM2
+
+	procesar_dos_pixeles									; modifica XMM0
+
+	movdqu 		[R9 + RCX], XMM0 							; escribo a destino
+
+.inc:
+	add 		RCX, 4
+	jmp 		.cond
+.endfor:
+
+	pop_regs
     ret
---
+++ solucion/src/handy.asm
@@ -0,0 +1,238 @@
+;	;	;	;	;	Datos ;	;	;	;	;	;
+
+section .data
+
+msj_debug:				DB 	'||||||||||||||||||DEBUG|||||||||||||||||||||||', 10, 0
+msj_debug_delim: 		DB 	'||||||||||||||||||||||||||||||||||||||||||||||', 10, 0
+fmt_debug_word:			DB 	'(word) %d', 10, 0
+fmt_debug_ubyte:		DB 	'(ubyte) %d', 10, 0
+fmt_debug_uint:			DB 	'(uint) %u', 10, 0
+
+;	;	;	;	;	Macros ;	;	;	;	;	;
+
+%macro alinear 0
+	sub		RSP, 8
+%endmacro
+
+%macro desalinear 0
+	add		RSP, 8
+%endmacro
+
+%macro push_regs 0
+	push 	RBP
+	mov		RBP, RSP
+	push 	RBX
+	push 	R12
+	push 	R13
+	push 	R14
+	push 	R15
+	alinear
+%endmacro
+
+%macro pop_regs 0
+	desalinear
+	pop 	R15
+	pop 	R14
+	pop 	R13
+	pop 	R12
+	pop 	RBX
+	pop 	RBP
+%endmacro
+
+%define OFFSET_OPS_STACK	64
+
+%macro push_xmm 1
+	sub 		RSP, 16
+	movdqu 		[RSP], %1
+%endmacro
+
+%macro pop_xmm 1
+	movdqu 		%1, [RSP]
+	add 		RSP, 16
+%endmacro
+
+%macro push_defensivo 0
+	push_xmm 	XMM0
+	push_xmm 	XMM1
+	push_xmm 	XMM2
+	push_xmm 	XMM3
+	push_xmm 	XMM4
+	push_xmm 	XMM5
+	push_xmm 	XMM6
+	push_xmm 	XMM7
+	push_xmm 	XMM8
+	push_xmm 	XMM9
+	push_xmm 	XMM10
+	push_xmm 	XMM11
+	push_xmm 	XMM12
+	push_xmm 	XMM13
+	push_xmm 	XMM14
+	push_xmm 	XMM15
+	push 		RAX
+	push 		RBX
+	push 		RCX
+	push 		RDX
+	push 		RDI
+	push 		RSI
+	push 		RBP
+	push 		RSP
+	push 		R8
+	push 		R9
+	push 		R10
+	push 		R11
+	push 		R12
+	push 		R13
+	push 		R14
+	push 		R15
+	alinear
+%endmacro
+
+%macro pop_defensivo 0
+	desalinear
+	pop 		R15
+	pop 		R14
+	pop 		R13
+	pop 		R12
+	pop 		R11
+	pop 		R10
+	pop 		R9
+	pop 		R8
+	pop 		RSP
+	pop 		RBP
+	pop 		RSI
+	pop 		RDI
+	pop 		RDX
+	pop 		RCX
+	pop 		RBX
+	pop 		RAX
+	pop_xmm 	XMM15
+	pop_xmm 	XMM14
+	pop_xmm 	XMM13
+	pop_xmm 	XMM12
+	pop_xmm 	XMM11
+	pop_xmm 	XMM10
+	pop_xmm 	XMM9
+	pop_xmm 	XMM8
+	pop_xmm 	XMM7
+	pop_xmm 	XMM6
+	pop_xmm 	XMM5
+	pop_xmm 	XMM4
+	pop_xmm 	XMM3
+	pop_xmm 	XMM2
+	pop_xmm 	XMM1
+	pop_xmm 	XMM0
+%endmacro
+
+%macro debug_xmm_ubytes 1
+	push_defensivo
+
+	debug_string msj_debug_delim
+
+	pextrb 		RDI, %1, 0
+	debug_ubyte DIL
+	pextrb 		RDI, %1, 1
+	debug_ubyte DIL
+	pextrb 		RDI, %1, 2
+	debug_ubyte DIL
+	pextrb 		RDI, %1, 3
+	debug_ubyte DIL
+	pextrb 		RDI, %1, 4
+	debug_ubyte DIL
+	pextrb 		RDI, %1, 5
+	debug_ubyte DIL
+	pextrb 		RDI, %1, 6
+	debug_ubyte DIL
+	pextrb 		RDI, %1, 7
+	debug_ubyte DIL
+	pextrb 		RDI, %1, 8
+	debug_ubyte DIL
+	pextrb 		RDI, %1, 9
+	debug_ubyte DIL
+	pextrb 		RDI, %1, 10
+	debug_ubyte DIL
+	pextrb 		RDI, %1, 11
+	debug_ubyte DIL
+	pextrb 		RDI, %1, 12
+	debug_ubyte DIL
+	pextrb 		RDI, %1, 13
+	debug_ubyte DIL
+	pextrb 		RDI, %1, 14
+	debug_ubyte DIL
+	pextrb 		RDI, %1, 15
+	debug_ubyte DIL
+
+	pop_defensivo
+%endmacro
+
+%macro debug_xmm_words 1
+	push_defensivo
+
+	debug_string msj_debug_delim
+
+	pextrw 		RDI, %1, 0
+	debug_word DI
+	pextrw 		RDI, %1, 1
+	debug_word DI
+	pextrw 		RDI, %1, 2
+	debug_word DI
+	pextrw 		RDI, %1, 3
+	debug_word DI
+	pextrw 		RDI, %1, 4
+	debug_word DI
+	pextrw 		RDI, %1, 5
+	debug_word DI
+	pextrw 		RDI, %1, 6
+	debug_word DI
+	pextrw 		RDI, %1, 7
+	debug_word DI
+
+	pop_defensivo
+%endmacro
+
+%macro debug_string 1
+	push_defensivo
+
+	mov  		RDI, %1
+	mov  		RAX, 0
+	call printf
+
+	pop_defensivo
+%endmacro
+
+%macro debug_word 1
+	push_defensivo
+
+	mov 		AX, %1
+	cwde
+	cdqe
+
+	mov  		RSI, RAX
+	mov  		RDI, fmt_debug_word
+	mov  		RAX, 0
+	call printf
+
+	pop_defensivo
+%endmacro
+
+%macro debug_uint 1
+	push_defensivo
+
+	mov  		RSI, %1
+	mov  		RDI, fmt_debug_uint
+	mov  		RAX, 0
+	call printf
+
+	pop_defensivo
+%endmacro
+
+%macro debug_ubyte 1
+	push_defensivo
+
+	xor 		RSI, RSI
+	mov  		SIL, %1
+	mov  		RDI, fmt_debug_ubyte
+	mov  		RAX, 0
+	call printf
+
+	pop_defensivo
+%endmacro
